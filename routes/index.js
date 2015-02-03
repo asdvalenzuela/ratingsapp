f9b9d3dae7d3ee9f3aed3bdbafc597549b0fc62a
Alaina <asdvalenzuela@gmail.com>
2015-02-02 21:31:50 -0800

    post route now working, users can add new restaurant ratings

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,31 @@ router.get('/newrating', function(req, res) {
     res.render('newrating', { title: 'Add New Rating' });
 });
 
+router.post('/addrating', function(req, res) {
+    var db = req.db;
+
+    var restaurantName = req.body.restaurantName;
+    var rating = req.body.rating;
+
+    var collection = db.get('Rating');
+
+    collection.insert({
+        "restaurant_name" : restaurantName,
+        "rating" : rating
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("ratingslist");
+            // And forward to success page
+            res.redirect("ratingslist");
+        }
+    });
+});
+
 module.exports = router;
 
 
